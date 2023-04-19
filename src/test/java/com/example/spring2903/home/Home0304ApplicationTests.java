e631a5928bdcb1d5b810431da6362ebb4b27bc76
Myroslava Shulimenko <mira.shulimenko@gmail.com>
2023-04-19 09:39:26 +0200

    home0304 обновила

---
+++ src/test/java/com/example/spring2903/home/Home0304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring2903.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home0304ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
